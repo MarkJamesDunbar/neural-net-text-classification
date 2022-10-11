25e89695382556d330d1e7b5f497ad3cfd3b5cab
Mark Dunbar <57494763+MerkuMerku@users.noreply.github.com>
2022-10-12 00:32:40 +0100

    minor classname refactor

--- src/kannadamnist_module/config/config.py
+++ src/kannadamnist_module/config/config.py
@@ -12,7 +12,7 @@ DIG_FILE="Dig-MNIST.csv"
 # Data Config
 SPLITSIZE = 0.2
 NUM_CLASSES = 10
-CLASS_NAMES = {0:"omdu", 1:"eradu", 2:"muru", 3:"nalku", 4:"aidu", 5:"aru", 6:"elu", 7:"emtu", 8:"ombattu", 9:"hattu"}
+CLASS_NAMES = {0:"Omdu", 1:"Eradu", 2:"Muru", 3:"Nalku", 4:"Aidu", 5:"Aru", 6:"Elu", 7:"Emtu", 8:"Ombattu", 9:"Hattu"}
 IMAGE_SIZE = 28
 
 # Model Config
