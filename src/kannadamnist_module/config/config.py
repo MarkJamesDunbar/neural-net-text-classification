09673a79dbdad0e5427244199884539edef4826e
Mark Dunbar <57494763+MerkuMerku@users.noreply.github.com>
2022-10-11 15:52:12 +0100

    tidy up config layout

--- src/kannadamnist_module/config/config.py
+++ src/kannadamnist_module/config/config.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 # Filepath Config
 DATAPATH="./datasets/"
 OUTPUT="output/"
@@ -10,16 +9,14 @@ TEST_FILE="test.csv"
 SAMPLE_FILE ="sample_submission.csv"
 DIG_FILE="Dig-MNIST.csv"
 
-
 # Data Config
 SPLITSIZE = 0.2
 NUM_CLASSES = 10
 CLASS_NAMES = {0:"omdu", 1:"eradu", 2:"muru", 3:"nalku", 4:"aidu", 5:"aru", 6:"elu", 7:"emtu", 8:"ombattu", 9:"hattu"}
 IMAGE_SIZE = 28
 
-
 # Model Config
 INITIAL_LR = 0.01
 BATCH_SIZE = 100
-EPOCHS = 10
+EPOCHS = 5
 
